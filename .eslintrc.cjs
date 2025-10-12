63524b20084867348c71b5cb1c0407078756767a
James Volpe <volpestyle@gmail.com>
2025-10-11 20:59:47 -0400

    chore: scaffold frontend workspace

---
+++ .eslintrc.cjs
@@ -0,0 +1,46 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: null
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'jsx-a11y',
+    'testing-library'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/strict',
+    'plugin:testing-library/react'
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
+  },
+  ignorePatterns: [
+    'dist',
+    'storybook-static',
+    'coverage',
+    '**/*.config.*',
+    '**/*.stories.tsx'
+  ]
+};
